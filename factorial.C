64cd913d261576e63b0d6bdb6742e085a145eaf0
Alejandra Chávez Cruz <42187730+alechavez26@users.noreply.github.com>
2018-09-07 04:49:15 -0500

    Add files via upload

---
+++ factorial.C
@@ -0,0 +1,25 @@
+﻿/*
+ * A01411970@itesm@.mx
+ * Alejandra Chávez Cruz
+ * 06/sept/18
+ */
+
+
+#include <stdio.h>
+
+int main() {
+    //Declaration of variables
+
+    int fact = 1;
+    int n;
+    int v = 1;
+
+    printf("Write a number\n");
+    scanf("%d",&fact);
+
+    for (1 = fact; v > 1; v--)
+        fact = fact * v
+}
+printf("The factorial of %d= %d\n",fact);
+
+}
